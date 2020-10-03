de3d51bb98966fefef7da7528d3a3cffcf9a86e9
dimonlev <dimon_lev@mail.ru>
2020-10-03 12:09:45 +0300

    init: start typical-arrays-problem-task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if ((array || []).length === 0) {
+    return 0
+  }
+  else {
+    let minElement = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (minElement > array[i]) {
+        minElement = array[i];
+      }
+    }
+    return minElement;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if ((array || []).length === 0) {
+    return 0
+  }
+  else {
+    let maxElement = array[0];
+    for (let i = 0; i < array.length; i++) {
+      if (maxElement < array[i]) {
+        maxElement = array[i];
+      }
+    }
+    return maxElement;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if ((array || []).length === 0) {
+    return 0
+  }
+  else {
+    let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+      sum = sum + array[i];
+    }
+    return sum / array.length;
+  }
 }
